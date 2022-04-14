f6cc549b20de24110849d922edd32e2a652f40e9
HaseFlrn <florianhase12@gmail.com>
2022-04-14 17:28:56 +0200

    fix trycatch

--- Router.java
+++ Router.java
@@ -1,14 +1,13 @@
+import Exceptions.InvalidMessage;
+import java.net.*;
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Map.Entry;
 
-import Exceptions.InvalidMessage;
-
-import java.net.*;
-
 public class Router extends Device {
+
   private HashMap<Integer, RoutingEntry> paths;
   private byte[] buffer;
   private int myDevicePort;
@@ -19,7 +18,13 @@ public class Router extends Device {
   private HashMap<String, Long> waitingForAck;
   private HashMap<String, Long> knownIds;
 
-  public Router(int xCoord, int yCoord, int port, Field field, int myDevicePort) {
+  public Router(
+    int xCoord,
+    int yCoord,
+    int port,
+    Field field,
+    int myDevicePort
+  ) {
     super(xCoord, yCoord, field, port);
     this.myDevicePort = myDevicePort;
     this.paths = new HashMap<>();
@@ -50,15 +55,15 @@ public class Router extends Device {
             for (int i = 0; i < toSend.length; i++) {
               socket.send(toSend[i]);
             }
-          } catch (SocketTimeoutException e) {
-            count++;
-            count = count % 10;
-            if( count == 0 ) {
-              checkAcks();
-            }
           } catch (InvalidMessage e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
+          }
+        } catch (SocketTimeoutException e) {
+          count++;
+          count = count % 10;
+          if (count == 0) {
+            checkAcks();
           }
         } catch (Exception e) {
           e.printStackTrace();
@@ -84,14 +89,18 @@ public class Router extends Device {
           toSend = this.processSend(msg);
           break;
         case RouteRequest:
-          if (msg.getDestPort() == this.myDevicePort && !knownIds.containsKey(msg.getMessageId())) {
+          if (
+            msg.getDestPort() == this.myDevicePort &&
+            !knownIds.containsKey(msg.getMessageId())
+          ) {
             knownIds.put(msg.getMessageId(), Instant.now().getEpochSecond());
             msg.setCommand(Command.RouteReply);
             msg.addToPath(this.port);
             msg.setDestPort(msg.getSourcePort());
             msg.setSourcePort(this.port);
             toSend = new DatagramPacket[1];
-            toSend[0] = this.createDatagramPacket(msg, this.getPreviousPort(msg));
+            toSend[0] =
+              this.createDatagramPacket(msg, this.getPreviousPort(msg));
           } else {
             toSend = this.processRouteRequest(msg);
           }
@@ -100,9 +109,16 @@ public class Router extends Device {
           if (msg.getDestPort() == this.port) {
             toSend = this.processRouteReply(msg);
           } else {
-            if(field.isRouterInRange(this.getPreviousPort(msg), this.xCoord, this.yCoord)) {
+            if (
+              field.isRouterInRange(
+                this.getPreviousPort(msg),
+                this.xCoord,
+                this.yCoord
+              )
+            ) {
               toSend = new DatagramPacket[1];
-              toSend[0] = this.createDatagramPacket(msg, this.getPreviousPort(msg));
+              toSend[0] =
+                this.createDatagramPacket(msg, this.getPreviousPort(msg));
             }
           }
           break;
@@ -110,8 +126,14 @@ public class Router extends Device {
           if (msg.getDestPort() == this.myDevicePort) {
             // weitere Code, falls Enddevice die Nachricht erreichen soll
             toSend = new DatagramPacket[1];
-            Message ack = new Message(msg.getMessageId(), Command.Ack, this.port, this.getPreviousPort(msg),
-                msg.getPath(), "");
+            Message ack = new Message(
+              msg.getMessageId(),
+              Command.Ack,
+              this.port,
+              this.getPreviousPort(msg),
+              msg.getPath(),
+              ""
+            );
             toSend[0] = this.createDatagramPacket(ack, ack.getDestPort());
             System.out.println("Router " + this.port + ": " + msg.getContent());
           } else {
@@ -153,7 +175,10 @@ public class Router extends Device {
       entry.updateUsage();
       msg.setPath(entry.getPath());
       msg.setSourcePort(port);
-      this.waitingForAck.put(msg.getMessageId(), Instant.now().getEpochSecond());
+      this.waitingForAck.put(
+          msg.getMessageId(),
+          Instant.now().getEpochSecond()
+        );
 
       packet[0] = createDatagramPacket(msg, getNextPort(msg));
     } else {
@@ -164,13 +189,22 @@ public class Router extends Device {
 
   public void groomPaths() {
     long currTime = Instant.now().getEpochSecond();
-    this.paths.entrySet().removeIf(e -> (currTime - e.getValue().getLastUsed() >= 300));
+    this.paths.entrySet()
+      .removeIf(e -> (currTime - e.getValue().getLastUsed() >= 300));
   }
 
-  public DatagramPacket[] createRouteRequest(Message msg) throws UnknownHostException {
+  public DatagramPacket[] createRouteRequest(Message msg)
+    throws UnknownHostException {
     LinkedList<Integer> list = new LinkedList<Integer>();
     list.add(this.port);
-    Message newMessage = new Message(getUUID(), Command.RouteRequest, this.port, msg.getDestPort(), list, "");
+    Message newMessage = new Message(
+      getUUID(),
+      Command.RouteRequest,
+      this.port,
+      msg.getDestPort(),
+      list,
+      ""
+    );
 
     msg.setCommand(Command.Forward);
     waiting.put(newMessage.getMessageId(), msg);
@@ -178,7 +212,8 @@ public class Router extends Device {
     return getMulticastPackets(newMessage);
   }
 
-  public DatagramPacket createDatagramPacket(Message msg, int port) throws UnknownHostException {
+  public DatagramPacket createDatagramPacket(Message msg, int port)
+    throws UnknownHostException {
     InetAddress destAddress = InetAddress.getByName("localhost");
     byte[] message = msg.toString().getBytes();
     return new DatagramPacket(message, message.length, destAddress, port);
@@ -208,10 +243,10 @@ public class Router extends Device {
     }
 
     return nextPort;
-
   }
 
-  public DatagramPacket[] processRouteRequest(Message msg) throws UnknownHostException {
+  public DatagramPacket[] processRouteRequest(Message msg)
+    throws UnknownHostException {
     DatagramPacket[] packet = new DatagramPacket[0];
     if (!knownIds.containsKey(msg.getMessageId())) {
       knownIds.put(msg.getMessageId(), Instant.now().getEpochSecond());
@@ -221,9 +256,11 @@ public class Router extends Device {
     return packet;
   }
 
-  public DatagramPacket[] getMulticastPackets(Message msg) throws UnknownHostException {
+  public DatagramPacket[] getMulticastPackets(Message msg)
+    throws UnknownHostException {
     DatagramPacket[] packet;
-    LinkedList<Router> reachable = this.field.getReachableRouter(this.port, this.xCoord, this.yCoord);
+    LinkedList<Router> reachable =
+      this.field.getReachableRouter(this.port, this.xCoord, this.yCoord);
     Iterator<Router> it = reachable.iterator();
     packet = new DatagramPacket[reachable.size()];
     int i = 0;
@@ -234,17 +271,19 @@ public class Router extends Device {
     return packet;
   }
 
-  public DatagramPacket[] processRouteReply(Message msg) throws UnknownHostException {
+  public DatagramPacket[] processRouteReply(Message msg)
+    throws UnknownHostException {
     DatagramPacket[] toSend = new DatagramPacket[1];
 
-    if(field.isRouterInRange(getNextPort(msg), this.xCoord, this.yCoord)) {
+    if (field.isRouterInRange(getNextPort(msg), this.xCoord, this.yCoord)) {
       //source port + 1 to get port of the receiver enddevice
       this.paths.put(msg.getSourcePort() + 1, new RoutingEntry(msg.getPath()));
       System.out.println(this.port + ": the routing path is " + msg.getPath());
       Message oldMessage = waiting.get(msg.getMessageId());
       waiting.remove(msg.getMessageId());
       oldMessage.setPath(msg.getPath());
-      toSend[0] = this.createDatagramPacket(oldMessage, getNextPort(oldMessage));
+      toSend[0] =
+        this.createDatagramPacket(oldMessage, getNextPort(oldMessage));
     } else {
       Message oldMessage = waiting.get(msg.getMessageId());
       toSend = createRouteRequest(oldMessage);
@@ -252,41 +291,70 @@ public class Router extends Device {
     return toSend;
   }
 
-  public DatagramPacket[] processForward(Message msg) throws UnknownHostException {
+  public DatagramPacket[] processForward(Message msg)
+    throws UnknownHostException {
     DatagramPacket[] toSend = new DatagramPacket[1];
     int nextRouter = this.getNextPort(msg);
     if (field.isRouterInRange(nextRouter, this.xCoord, this.yCoord)) {
-      if (field.isRouterInRange(this.getPreviousPort(msg), this.xCoord, this.yCoord)) {
+      if (
+        field.isRouterInRange(
+          this.getPreviousPort(msg),
+          this.xCoord,
+          this.yCoord
+        )
+      ) {
         toSend = new DatagramPacket[2];
-        Message ack = new Message(msg.getMessageId(), Command.Ack, this.port, this.getPreviousPort(msg), msg.getPath(),
-            "");
+        Message ack = new Message(
+          msg.getMessageId(),
+          Command.Ack,
+          this.port,
+          this.getPreviousPort(msg),
+          msg.getPath(),
+          ""
+        );
         toSend[1] = this.createDatagramPacket(ack, ack.getDestPort());
       }
       toSend[0] = this.createDatagramPacket(msg, nextRouter);
       waitingForAck.put(msg.getMessageId(), Instant.now().getEpochSecond());
     } else {
       String content = msg.getDestPort() + " " + msg.getMessageId();
-      Message errMessage = new Message(getUUID(), Command.RouteError, this.port, msg.getSourcePort(), msg.getPath(), content );
-      toSend[0] = createDatagramPacket(errMessage, this.getPreviousPort(errMessage));
+      Message errMessage = new Message(
+        getUUID(),
+        Command.RouteError,
+        this.port,
+        msg.getSourcePort(),
+        msg.getPath(),
+        content
+      );
+      toSend[0] =
+        createDatagramPacket(errMessage, this.getPreviousPort(errMessage));
     }
     return toSend;
   }
 
-  public DatagramPacket[] processRouteError(Message msg) throws UnknownHostException{
+  public DatagramPacket[] processRouteError(Message msg)
+    throws UnknownHostException {
     DatagramPacket[] toSend = new DatagramPacket[1];
 
-    String[] contentParts = msg.getContent().split(" ",2);
+    String[] contentParts = msg.getContent().split(" ", 2);
     this.paths.remove(Integer.parseInt(contentParts[0]));
-    Message retryMessage = new Message(getUUID(), Command.Retry, this.port, this.myDevicePort, new LinkedList<>(), contentParts[1]); 
+    Message retryMessage = new Message(
+      getUUID(),
+      Command.Retry,
+      this.port,
+      this.myDevicePort,
+      new LinkedList<>(),
+      contentParts[1]
+    );
 
     LinkedList<Integer> newPath = new LinkedList<>();
     // destPort -1 to get the routerPort (routerPort ist in dem path gespeichert)
     int destPort = Integer.parseInt(contentParts[0]) - 1;
-    for( Entry<Integer, RoutingEntry> e: this.paths.entrySet()) {
+    for (Entry<Integer, RoutingEntry> e : this.paths.entrySet()) {
       RoutingEntry re = e.getValue();
-      if( isInPath(re.getPath(), destPort)) {
+      if (isInPath(re.getPath(), destPort)) {
         newPath = getSubPath(re.getPath(), destPort);
-        
+
         //destPort + 1 to get the EnddevicePort
         this.paths.put(destPort + 1, new RoutingEntry(newPath));
         break;
@@ -298,31 +366,32 @@ public class Router extends Device {
   }
 
   public void checkAcks() {
-    for( Entry<String, Long> e: waitingForAck.entrySet()) {
+    for (Entry<String, Long> e : waitingForAck.entrySet()) {
       long currTime = Instant.now().getEpochSecond();
-      if(currTime - e.getValue() >= 300) {
-        
-      }
+      if (currTime - e.getValue() >= 300) {}
     }
   }
 
   public boolean isInPath(LinkedList<Integer> path, int port) {
     Iterator<Integer> it = path.iterator();
-    while( it.hasNext() ) {
-      if(it.next() == port ) {
+    while (it.hasNext()) {
+      if (it.next() == port) {
         return true;
       }
     }
     return false;
   }
 
-  public LinkedList<Integer> getSubPath(LinkedList<Integer> longPath, int destPort) {
+  public LinkedList<Integer> getSubPath(
+    LinkedList<Integer> longPath,
+    int destPort
+  ) {
     LinkedList<Integer> newPath = new LinkedList<>();
     Iterator<Integer> it = longPath.iterator();
-    while ( it.hasNext() ) {
+    while (it.hasNext()) {
       int currPort = it.next();
       newPath.add(currPort);
-      if( currPort == destPort) {
+      if (currPort == destPort) {
         break;
       }
     }
@@ -331,7 +400,9 @@ public class Router extends Device {
 
   public void updateRoutePaths() {
     for (Entry<Integer, RoutingEntry> path : this.paths.entrySet()) {
-      if (Instant.now().getEpochSecond() - path.getValue().getLastUsed() >= 600) {
+      if (
+        Instant.now().getEpochSecond() - path.getValue().getLastUsed() >= 600
+      ) {
         this.paths.remove(path.getKey());
       }
     }
@@ -356,5 +427,4 @@ public class Router extends Device {
   public void setYCoord(int yCoord) {
     this.yCoord = yCoord;
   }
-
 }
